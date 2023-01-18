c8a758816ce7b20de54146f789c4540d5ec4bd64
Danilo Franceschi <danfranceschi231@gmail.com>
2023-01-17 21:20:55 -0300

    Criada configuração inicial de producer e consumer kafka

--- src/main/java/com/queue/hospital/QueueHospital/adapters/rest/UserControler.java
+++ src/main/java/com/queue/hospital/QueueHospital/adapters/rest/UserControler.java
@@ -1,6 +1,9 @@
 package com.queue.hospital.QueueHospital.adapters.rest;
 
+import com.queue.hospital.QueueHospital.usecases.SendInfoQueueHospital;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("user")
 public class UserControler {
 
+    @Autowired
+    SendInfoQueueHospital sendInfoQueueHospital;
     @PostMapping
-    public String storeUser(String body){
+    public String storeUser(@RequestBody String body){
+        System.out.print("request user post " + body);
+        sendInfoQueueHospital.send(body);
         return "111";
     }
 
